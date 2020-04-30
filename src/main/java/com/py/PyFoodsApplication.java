b59ce2b0e5bf515f5b7801c01d26e8cce1f2d365
ltbhoan <ltbhoan@hanatourjsv.com>
2020-04-30 23:54:52 +0700

    - api - allow all access origin

--- src/main/java/com/py/PyFoodsApplication.java
+++ src/main/java/com/py/PyFoodsApplication.java
@@ -4,6 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PyFoodsApplication {
@@ -15,5 +17,15 @@ public class PyFoodsApplication {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 }
